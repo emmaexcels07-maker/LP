89ead284682c3e38748557372a1739aef9bd7ac2
emmaexcels07-maker <emmaexcels07@gmail.com>
2025-10-07 12:19:02 +0100

    Describe the changes made

---
+++ indexs.js
@@ -0,0 +1,35 @@
+function welcomeMessage(name) {
+    const message = `Welcome to the world of programing, ${name}!`; 
+    console.log(message);
+}
+welcomeMessage("Alice");
+
+    function addition(firstNumber, secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+ 
+    const weather = {
+        temperature: 22,
+        humidity: 60,
+        description: "Partly cloudy"
+    };  
+
+    console.log(`The weather is ${weather.description} with a temperature of ${weather.temperature}°C and humidity of ${weather.humidity}%.`);
+
+    function multiplication(a, b) {
+        return a * b;
+    }
+    console.log(multiplication(5, 3));
+
+    function division(a, b) {
+        let divide = a / b;
+        return divide;
+    }   
+    console.log(division(10, 2));
+
+    function subtraction(a, b) {
+        return a - b;
+    }   
+    console.log(subtraction(10, 4));
+
