b4ee1810d7a3442d494b15a250d45ad92709bde6
emmaexcels07-maker <emmaexcels07@gmail.com>
2025-10-07 11:46:47 +0100

    Add new code

---
+++ index1.js
@@ -0,0 +1,26 @@
+let selectedcolors = ['red', 'blue'];
+selectedcolors[2] = 'yellow';
+
+
+
+
+const classcourse = {
+    coursename: 'JavaScript',
+    courseprice: 299,
+    courseinstructor: 'John Doe'
+};
+
+const mixedarray = [2007 , 'Hello', true, { name: 'Alice' }, [1, 2, 3]];
+
+
+const friuts = ['Apple', 'Banana', 'Orange'];
+friuts.pop();
+
+const person = {
+    name: 'Emmanuel Feyisayo',
+    age: 25,
+    occupation: 'Software Developer',   
+    country: 'Nigeria',
+    city: 'Lagos'
+};
+console.log(person);
---
+++ index2.js
@@ -0,0 +1,28 @@
+
+function addition(fn,sn){
+    const formula = fn + sn;
+    return formula;
+
+}
+const data = addition(400,600);
+
+
+function multiplication(fn,sn){
+    const formula = fn * sn;
+    return formula;
+}
+const data2 = multiplication(400,600);
+
+function subtration (fn,sn){
+    const formula =fn - sn;
+    return formula;
+}
+const data3 = subtration(400,600);
+
+function division(fn,sn){
+    const formula =fn / sn;
+    return formula;
+}
+const data4 = division(400,600);
+
+console.log (`The data variables are: ${data}, ${data2}, ${data3}, ${data4}`);
