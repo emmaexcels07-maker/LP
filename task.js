af743767b7f85e055dc09aa7a329c823fe947c3e
emmaexcels07-maker <emmaexcels07@gmail.com>
2025-10-13 13:56:29 +0100

    Describe the changes made

---
+++ task.js
@@ -0,0 +1,55 @@
+// const friuts = ['Apple', 'Banana', 'Orange'];
+// friuts.push('watermeolon')
+// friuts.unshift('mango')
+// friuts.pop()
+// console.log(friuts);
+
+// let friuts = ['strawberry', 'banana', 'apple', 'blueberry', 'grapes'];
+// let longNameFruits = friuts.filter(friut => friut.length > 5);
+// // console.log(longNameFruits);
+
+// friuts = friuts.concat( longNameFruits);
+// friuts.sort();
+// console.log(friuts);
+
+// let num = [5, 10, 17]
+// num. forEach(function checkNumber(num) {
+//     if (num % 2 === 0) {
+//         console.log(`The number ${num} is even.`);
+//     } else {
+//         console.log(`The number ${num} is odd.`)
+//     }
+// }) 
+
+
+
+// let num = [5, 10, 17]
+// function checkNumber(num) {
+//     if (num % 2 === 0) {
+//         return "Number is even";
+//     } else {
+//         return "Number is odd";
+//     }
+// }
+// console.log(checkNumber(5));
+// console.log(checkNumber(10));
+// console.log(checkNumber(17));
+
+// for (let i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+
+// let i=1;
+// while (i<=20) {
+//     if (i %2 ===0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// const nameArray = ['Ayo', 'Prisca', 'John'];
+// nameArray.forEach(name => {
+//   console.log(name.toUpperCase());
+// });
